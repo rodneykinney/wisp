4ff3bd462d3768ef518ce3ccb66570dcdc3376b9
Rodney Kinney <rodney.m.kinney@gmail.com>
2015-04-13 22:50:38 -0700

    Refactor

--- core/src/main/scala/Scratch.scala
+++ core/src/main/scala/Scratch.scala
@@ -2,10 +2,21 @@
  * Created by rodneykinney on 4/13/15.
  */
 object Scratch {
+
   import com.quantifind.charts.Highcharts._
+
   def main(args: Array[String]): Unit = {
     val x = (0 to 100).map(_ * .01)
+    //    line(x.toArray, x.toArray)
+//    implicit def arrToI[T](arr: Array[T]) = new PairFromIterable(arr.toIterable)
     line(x)
+    line(x.toArray[Double])
+    line(x, x)
+    line(x, (t: Double) => t * t)
+    line((t: Double) => t * t, x)
+    histogram(x)
+    column(x.map(_.toString), x)
+    pie(x.map(_.toString), x)
   }
 
 }
--- core/src/main/scala/com/quantifind/charts/Highcharts.scala
+++ core/src/main/scala/com/quantifind/charts/Highcharts.scala
@@ -11,27 +11,33 @@ import scala.language.implicitConversions
  *
  * Highcharts implementation of plotting functionality. Includes several highcharts specific plots
  *
- * I rely on the fact that an implicit method defined in an object takes precedence over one
- * defined in a trait to have Iterable[T] with PartialFunction[Int, T] resolve to this method
  */
 
-object Highcharts extends IterablePairLowerPriorityImplicits with BinnedDataLowerPriorityImplicits with HighchartsStyles {
+object Highcharts extends BinnedDataLowerPriorityImplicits with HighchartsStyles {
 
   implicit class PairFromStringAndIterable[B](ab: (Iterable[String], Iterable[B])) extends
   IterablePair[String, B] with HasCategories {
-      def toIterables: (Iterable[String], Iterable[B]) = (ab._1, ab._2)
-      def getCategories: Iterable[String] = ab._1
-    }
+    def toIterables: (Iterable[String], Iterable[B]) = (ab._1, ab._2)
 
-  implicit class PairFromIterableStringTuple[B](ab: (Iterable[(String, B)]))  extends
+    def getCategories: Iterable[String] = ab._1
+  }
+
+  implicit class PairFromIterableStringTuple[B](ab: (Iterable[(String, B)])) extends
   IterablePair[String, B] with HasCategories {
     def toIterables: (Iterable[String], Iterable[B]) = (ab.map(_._1), ab.map(_._2))
+
     def getCategories: Iterable[String] = ab.map(_._1)
   }
 
-//  implicit def mkStringArrayArray[B](ab: (Array[String], Array[B])) = new StringIterableIterable(ab._1.toSeq, ab._2.toSeq)
-//
-//  implicit def mkStringArrayArray[B](ab: (Array[(String, B)])) = new StringIterableIterable(ab.map(_._1).toSeq, ab.map(_._2).toSeq)
+  implicit class PairFromIterableFunction[A, B](ab: (Iterable[A], A => B)) extends
+  IterablePair[A, B] {
+    def toIterables: (Iterable[A], Iterable[B]) = (ab._1, ab._1.map(ab._2))
+  }
+
+  implicit class PairFromFunctionIterable[A, B](ab: (A => B, Iterable[A])) extends
+  IterablePair[A, B] {
+    def toIterables: (Iterable[A], Iterable[B]) = (ab._2, ab._2.map(ab._1))
+  }
 
   implicit class PairFromPairOfIterables[A, B](ab: (Iterable[A], Iterable[B])) extends IterablePair[A, B] {
     def toIterables: (Iterable[A], Iterable[B]) = (ab._1, ab._2)
@@ -41,7 +47,11 @@ object Highcharts extends IterablePairLowerPriorityImplicits with BinnedDataLowe
     def toIterables: (Iterable[A], Iterable[B]) = (ab.map(_._1), ab.map(_._2))
   }
 
-  implicit class PairFromIterable[B](b: (Iterable[B]))extends IterablePair[Int, B] {
+  implicit class PairFromIterable[B](b: (Iterable[B])) extends IterablePair[Int, B] {
+    def toIterables = ((0 until b.size), b)
+  }
+
+  implicit class PairFromArray[B](b: (Array[B])) extends IterablePair[Int, B] {
     def toIterables = ((0 until b.size), b)
   }
 
