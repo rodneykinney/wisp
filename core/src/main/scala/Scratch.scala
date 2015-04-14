790d114fd7557ddb42c4ea3f16550489b2497908
Rodney Kinney <rodney.m.kinney@gmail.com>
2015-04-13 23:00:24 -0700

    Refactor

--- core/src/main/scala/Scratch.scala
+++ core/src/main/scala/Scratch.scala
@@ -7,16 +7,22 @@ object Scratch {
 
   def main(args: Array[String]): Unit = {
     val x = (0 to 100).map(_ * .01)
-    //    line(x.toArray, x.toArray)
-//    implicit def arrToI[T](arr: Array[T]) = new PairFromIterable(arr.toIterable)
     line(x)
-    line(x.toArray[Double])
     line(x, x)
     line(x, (t: Double) => t * t)
     line((t: Double) => t * t, x)
     histogram(x)
     column(x.map(_.toString), x)
     pie(x.map(_.toString), x)
+
+    val a = x.toArray
+    line(a)
+    line(a, a)
+    line(a, (t: Double) => t * t)
+    line((t: Double) => t * t, a)
+//    histogram(a)
+    column(a.map(_.toString), a)
+    pie(a.map(_.toString), a)
   }
 
 }
--- core/src/main/scala/com/quantifind/charts/Highcharts.scala
+++ core/src/main/scala/com/quantifind/charts/Highcharts.scala
@@ -29,29 +29,53 @@ object Highcharts extends BinnedDataLowerPriorityImplicits with HighchartsStyles
     def getCategories: Iterable[String] = ab.map(_._1)
   }
 
-  implicit class PairFromIterableFunction[A, B](ab: (Iterable[A], A => B)) extends
-  IterablePair[A, B] {
+  implicit class PairFromIterableFunction[A, B](ab: (Iterable[A], A => B))
+    extends IterablePair[A, B] {
     def toIterables: (Iterable[A], Iterable[B]) = (ab._1, ab._1.map(ab._2))
   }
 
-  implicit class PairFromFunctionIterable[A, B](ab: (A => B, Iterable[A])) extends
-  IterablePair[A, B] {
+  implicit class PairFromArrayFunction[A, B](ab: (Array[A], A => B))
+    extends IterablePair[A, B] {
+    def toIterables: (Iterable[A], Iterable[B]) = (ab._1, ab._1.map(ab._2))
+  }
+
+  implicit class PairFromFunctionArray[A, B](ab: (A => B, Array[A]))
+    extends IterablePair[A, B] {
     def toIterables: (Iterable[A], Iterable[B]) = (ab._2, ab._2.map(ab._1))
   }
 
-  implicit class PairFromPairOfIterables[A, B](ab: (Iterable[A], Iterable[B])) extends IterablePair[A, B] {
+  implicit class PairFromFunction[A, B](ab: (A => B, Iterable[A]))
+    extends IterablePair[A, B] {
+    def toIterables: (Iterable[A], Iterable[B]) = (ab._2, ab._2.map(ab._1))
+  }
+
+  implicit class PairFromPairOfIterables[A, B](ab: (Iterable[A], Iterable[B]))
+    extends IterablePair[A, B] {
     def toIterables: (Iterable[A], Iterable[B]) = (ab._1, ab._2)
   }
 
-  implicit class PairFromIterableTuple[A, B](ab: (Iterable[(A, B)])) extends IterablePair[A, B] {
+  implicit class PairFromPairOfArrays[A, B](ab: (Array[A], Array[B]))
+    extends IterablePair[A, B] {
+    def toIterables: (Iterable[A], Iterable[B]) = (ab._1, ab._2)
+  }
+
+  implicit class PairFromIterableTuple[A, B](ab: (Iterable[(A, B)]))
+    extends IterablePair[A, B] {
     def toIterables: (Iterable[A], Iterable[B]) = (ab.map(_._1), ab.map(_._2))
   }
 
-  implicit class PairFromIterable[B](b: (Iterable[B])) extends IterablePair[Int, B] {
+  implicit class PairFromArrayTuple[A, B](ab: (Array[(A, B)]))
+    extends IterablePair[A, B] {
+    def toIterables: (Iterable[A], Iterable[B]) = (ab.map(_._1), ab.map(_._2))
+  }
+
+  implicit class PairFromIterable[B](b: (Iterable[B]))
+    extends IterablePair[Int, B] {
     def toIterables = ((0 until b.size), b)
   }
 
-  implicit class PairFromArray[B](b: (Array[B])) extends IterablePair[Int, B] {
+  implicit class PairFromArray[B](b: (Array[B]))
+    extends IterablePair[Int, B] {
     def toIterables = ((0 until b.size), b)
   }
 
