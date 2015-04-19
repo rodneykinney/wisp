6d1e94ab4be053570cdf41ba84b90afba1df8675
Rodney Kinney <rodney.m.kinney@gmail.com>
2015-04-19 09:32:30 -0700

    Clean up HTML rendering code

--- core/src/main/scala/wisp/highcharts/HighchartsHtmlDisplay.scala
+++ core/src/main/scala/wisp/highcharts/HighchartsHtmlDisplay.scala
@@ -17,10 +17,7 @@ class HighchartsHtmlDisplay extends HtmlChartDisplay[RootChart, RootConfig] {
 
   def renderChartsToHtml(): String = {
 
-    val sb = new StringBuilder()
-
-    val jsHeader =
-      """
+      s"""
         |<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
         |<html>
         |  <head>
@@ -28,43 +25,27 @@ class HighchartsHtmlDisplay extends HtmlChartDisplay[RootChart, RootConfig] {
         |      HighchartAPI
         |    </title>
         |    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-      """.stripMargin
-
-    sb.append(jsHeader)
-
-    val imports: String =
-      """
+        |
         |<script type="text/javascript" src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
         |<script type="text/javascript" src="http://code.highcharts.com/4.0.4/highcharts.js"></script>
         |<script type="text/javascript" src="http://code.highcharts.com/4.0.4/modules/exporting.js"></script>
         |<script type="text/javascript" src="http://code.highcharts.com/4.0.4/highcharts-more.js"></script>
-      """.stripMargin
-
-    sb.append(imports)
-
-    def reloadJs =
-      """
+        |
         |<script type="text/javascript">
         |var contentHash = 'HASH_PLACEHOLDER';
-        |$.ajax({
+        |$$.ajax({
         |  url: '/check',
         |  data: {'clientContentHash' : [contentHash]},
         |  success: function(result) {
         |    location.reload();
         |  }})
         |</script>
-      """.stripMargin
-
-
-    sb.append(reloadJs)
-
-    sb.append("</head>")
-    sb.append("<body>")
-    chartConfigs.map(renderChart).foreach(sb.append)
-    sb.append("</body>")
-    sb.append("</html>")
-
-    sb.toString()
+        |</head>
+        |<body>
+        |${chartConfigs.map(renderChart).mkString("\n")}
+        |</body>
+        |</html>
+        |""".stripMargin
   }
 
 
@@ -73,15 +54,13 @@ class HighchartsHtmlDisplay extends HtmlChartDisplay[RootChart, RootConfig] {
     val containerId = json.hashCode.toHexString
     s"""
        |<div id="container$containerId"></div>
-                                        |   <script type="text/javascript">
-                                        |       $$(function() {
-                                                    |           $$('#container$containerId').highcharts(
-                                                                                            |               $json
-        |           );
-        |       });
-        |   </script>
-        |
-        """.stripMargin
+       |<script type="text/javascript">
+       |  $$(function() {
+       |    $$('#container$containerId').highcharts(
+       |      $json
+       |    );
+       |  });
+       |</script>""".stripMargin
   }
 
 }
